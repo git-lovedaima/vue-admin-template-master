0a68ce4740c32a54be5c734a302476ffe2831c9a
liudeqi <agqwertyuiop@outlook.com>
2022-08-27 19:44:37 +0800

    feat:手机号和密码的校验以及更改了eslint规则

--- src/utils/validate.js
+++ src/utils/validate.js
@@ -1,3 +1,11 @@
+/*
+ * @Author: liudeqi agqwertyuiop@outlook.com
+ * @Date: 2022-08-25 09:06:01
+ * @LastEditors: liudeqi agqwertyuiop@outlook.com
+ * @LastEditTime: 2022-08-27 19:37:17
+ * @FilePath: \vue-admin-template-master\src\utils\validate.js
+ * @Description:
+ */
 /**
  * Created by PanJiaChen on 16/11/18.
  */
@@ -17,4 +25,18 @@ export function isExternal(path) {
 export function validUsername(str) {
   const valid_map = ['admin', 'editor']
   return valid_map.indexOf(str.trim()) >= 0
+}
+
+/**
+ * 手机号验证
+ */
+export function validMobile(str) {
+  return /^1[3-9]\d{9}$/.test(str) // 校验手机号
+}
+
+/**
+ * 密码验证
+ */
+export function validPassword(str) {
+  return /^\d{6,16}$/.test(str) // 校验手机号
 }
